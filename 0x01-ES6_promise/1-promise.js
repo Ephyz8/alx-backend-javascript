bd965cfaa7b43b0c6f742f054a56292bf46964ca
“Ephyz8” <ziwoyaephraim@gmail.com>
2024-06-26 21:39:47 +0200

    Task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise ((resolve, reject) => {
-    if (success) {
+    if (success === true) {
       resolve ({
         status: 200,
         body: 'Success'
@@ -10,5 +10,4 @@ export default function getFullResponseFromAPI(success) {
       reject(new Error('The fake API is not working currently'));
     }
   });
-
 }

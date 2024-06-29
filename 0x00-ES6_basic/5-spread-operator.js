1e054e3ced370aa2e56713e924e24978d214f63f
“Ephyz8” <ziwoyaephraim@gmail.com>
2024-06-29 16:45:47 +0200

    spread syntax

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,3 +1,3 @@
-export default function concatArrays(array1, array2, ...string) {
-  return string.join(array1, array2);
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string];
 }
